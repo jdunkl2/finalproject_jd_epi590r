de29e50fdf0e59e2dd398883897d13c3b0ca1fca
Janae Dunkley <janae.dunkley@emory.edu>
2025-08-23 18:49:12 -0400

    completing objective 2 & 3

--- R/epi590r_finalproject.R
+++ R/epi590r_finalproject.R
@@ -3,13 +3,14 @@ install.packages("here")
 install.packages("gtsummary")
 install.packages("gtsummary", dependencies = TRUE)
 install.packages("dplyr")
+install.packages("ggplot2")
 library(gtsummary)
 library(tidyverse)
 library(here)
 library(dplyr)
+library(ggplot2)
 
-
-
+#Importing and cleaning data
 penguins <- read_csv(here::here("data", "penguins_size.csv"))
 penguins <- penguins %>%
 	mutate(sex = na_if(sex, "."))
@@ -37,6 +38,33 @@ tbl_summary(
 	bold_labels() |>
 	modify_header(label = "**Variable**")
 
+#Objective 2: Fit a regression and present well-formatted results from the
+#regression (1 pt)
+#The regression doesn’t have to be of any particular scientific interest,
+#and you don’t have to interpret it in any particular way
+#You may use {gtsummary} or {broom} or both
 
+linear_model <- lm(flipper_length_mm ~ culmen_length_mm + culmen_depth_mm,
+									 data = penguins)
 
+summary(linear_model)
 
+tbl_regression(
+	linear_model,
+	intercept = TRUE,
+	label = list(
+		culmen_length_mm ~ "Culmen Length (mm)",
+		culmen_depth_mm ~ "Culmen depth (mm)"
+	))
+
+#For each 1-unit increase in culmen length, flipper length increases by 1.4
+#if culmen depth is held constant. Significant (p < 0.001)
+
+#Create a figure (1 pt)
+#It doesn’t need to look pretty; feel free to adapt some of the examples from
+#class, which were as simple as hist(data$variable) and as complicated as the
+#forest plot in the {broom} section
+#Feel free to look at some of the ggplot2 material from another course I taught
+
+ggplot(penguins, aes(x=island, fill=species))+
+	geom_bar(position="fill")
